79fa934ad9e5057d319fd21eccf03a44c036538a
Patrick Sier <Sier-Patrick@norc.org>
2016-09-18 17:06:23 -0500

    basic spatial join working on zip code geojson

--- nexrad-etl/pyspark-nexrad-etl.py
+++ nexrad-etl/pyspark-nexrad-etl.py
@@ -12,6 +12,52 @@ sqlContext = SQLContext(sc)
 
 from metpy.io.nexrad import Level2File
 from pyproj import Geod
+import geojson
+
+# From http://geospatialpython.com/2011/01/point-in-polygon.html
+def point_in_poly(x,y,poly):
+    n = len(poly)
+    inside = False
+
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
+
+# Spatial join seems unnecessary with small number of points,
+# doing a mock instead that returns the name of the shape if contained
+def spatial_join_mock(x,y,poly_list):
+    for poly in poly_list:
+        if point_in_poly(x,y,poly[1]):
+            return poly[0]
+    return ''
+
+# Used to filter for bounding box
+def in_bbox(x,y):
+    in_box = ((y <= 42.0231311) and
+              (y >= 41.644335) and
+              (x <= -87.524044) and
+              (x >= -87.940267))
+    return in_box
+
+def read_nexrad(fn, f):
+  nex = f
+  if fn.endswith('.gz'):
+    nex = zlib.decompress(nex,15+32)
+  try:
+    nex = Level2File(BytesIO(nex))
+  except:
+    nex = 0
+  return nex
 
 def process_nexrad(fn, f):
     sweep = 0
@@ -48,26 +94,52 @@ def process_nexrad(fn, f):
     timestamp_int = int(re.search(r'\d{8}_\d{6}',fn).group().replace('_',''))
     time_arr = np.ones([len(az),len(ref_range)])*timestamp_int
     # Reducing dimensionality into rows of timestamp, lat, lon, and data
-    arr_rows = np.dstack((time_arr,lat,lon,data))
+    arr_rows = np.dstack((time_arr,lon,lat,data))
     arr_simp = arr_rows.reshape(-1,4)
     # Remove any nan values to reduce size
     return arr_simp[~np.isnan(arr_simp).any(1)]
 
+# Loading zips into list of tuples with zip code and MultiPolygon
+with open('chi_zips.geojson','r') as cg:
+    chi_zips = geojson.load(cg)
+
+zip_tuples = list()
+for feat in chi_zips['features']:
+    shapes = feat['geometry']['coordinates'][0]
+    poly_tuples = [(x[0],x[1]) for x in shapes]
+    zip_code = feat['properties']['zip']
+    zip_tuples.append((zip_code,poly_tuples))
+
+# zip_fields = [StructField('zip',StringType(),True),
+#               StructField('shape',ArrayType(StringType),True)]
+# zip_schema = StructType(zip_schema)
+# zip_df = sqlContext.createDataFrame(zip_tuples,zip_schema)
+
 sc._jsc.hadoopConfiguration().set('fs.s3n.awsAccessKeyId', os.getenv('AWS_ACCESS_KEY_ID'))
 sc._jsc.hadoopConfiguration().set('fs.s3n.awsSecretAccessKey',os.getenv('AWS_SECRET_ACCESS_KEY'))
 s3nRdd = sc.binaryFiles('s3n://noaa-nexrad-level2/2006/07/09/KLOT/KLOT20060709_000601.gz')
-# Passing tuples through so that filename can be preserved
-s3bin_res = s3nRdd.map(lambda x: (x[0], zlib.decompress(x[1],15+32))
-                      ).map(lambda x: (x[0],Level2File(BytesIO(x[1])))
-                      ).map(lambda x: process_nexrad(x[0],x[1])
-                      ).flatMap(lambda x: x)
 
-nexrad_schema_str = 'timestamp lat lon precip'
+# Passing tuples through so that filename can be preserved
+s3bin_res = s3nRdd.map(lambda x: (x[0],read_nexrad(x[1]))
+                      ).filter(lambda x: isinstance(x[1],Level2File)
+                      ).map(lambda x: process_nexrad(x[0],x[1])
+                      ).flatMap(lambda x: x
+                      ).map(lambda x: (int(x[0]),float(x[1]),float(x[2]),float(x[3]))
+                      ).filter(lambda x: in_bbox(x[1],x[2])
+                      ).map(lambda x: (x[0],x[1],x[2],x[3],spatial_join_mock(x[1],x[2],zip_tuples))
+                      ).filter(lambda x: x[4] != '')
+
 # Convert to tuple with native Python data types for DataFrame
 nexrad_data_tuples = s3bin_res.map(lambda x: (int(x[0]),float(x[1]),float(x[2]),float(x[3])))
-fields = [StructField(field_name, StringType(), True) for field_name in nexrad_schema_str.split()]
-schema = StructType(fields)
+nexrad_fields = [StructField('timestamp',LongType(),True),
+                 StructField('lon',FloatType(),True),
+                 StructField('lat',FloatType(),True),
+                 StructField('precip',FloatType(),True),
+                 StructField('zip',StringType(),True)]
+nexrad_schema = StructType(nexrad_fields)
+
 # Creating DataFrames https://spark.apache.org/docs/2.0.0-preview/sql-programming-guide.html#programmatically-specifying-the-schema
-nexrad_df = sqlContext.createDataFrame(nexrad_data_tuples, schema)
+nexrad_df = sqlContext.createDataFrame(nexrad_data_tuples, nexrad_schema)
 print(nexrad_df.columns)
 print(nexrad_df.count())
+print(nexrad_df.show())
