df68ecb4e9d615402805cec54173769b186e5ca2
Scott Beslow <beslow-scott@norc.org>
2016-09-26 11:41:56 -0500

    Collect data from csv files for JS processing

--- sewer-overflows/js/csoData.js
+++ sewer-overflows/js/csoData.js
@@ -1,56 +1,73 @@
-var csoLatLongs = [];
+var csoLatLongs = {};
 var csoEvents = [];
 
 function readCsoPoints() {
     $.ajax({
         url: "data/outfall_points.csv",
         async: false,
+        dataType: "text",
         success: function (csvd) {
             var csoPoints = $.csv.toObjects(csvd);
             for (var i = 0; i < csoPoints.length; i++) {
-    }
-            readCsoEvents(csoPoints);
-            alert(JSON.stringify(data));
+                csoPoint = csoPoints[i];
+                csoLatLongs[csoPoint.outfall_name] = csoPoint;
+            }
+            drawMap();
         },
         error: function (xhr, textStatus, errorThrown) {
-            alert('request failed');
-        },
-        dataType: "text",
+            alert('Failed to retrieve lat/longs: ' + textStatus);
+        }
 
-    });
 
+    })
 }
 
 function readCsoEvents(csoPoints) {
     $.ajax({
+        url: "data/merged_cso_data.csv",
+        async: false,
+        dataType: "text",
+        success: function (csvd) {
+            csoEvents = $.csv.toObjects(csvd);
+            drawMap();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert('Failed to retrieve cso events: ' + textStatus);
+        }
+
+
+    })
+
+
+    $.ajax({
         dataType: 'json',
         url: '/json/cumulative_outfall_duration.json',
         success: function (data) {
-            for (var i = 0; i < data.length; i++) {
-                
-                var color = 'blue';
-                if (i < 10) {
-                    color = 'red';
-                }
-                else {
-                    continue;
-                }
 
-                var circle = L.circle([data[i].lat, data[i].lon], 500, {
-                    color: color,
-                    fillColor: color,
-                    fillOpacity: 1
-                }).addTo(mymap);
-
-            }
         }
     });
 
 }
 
 function drawMap() {
-    if ((csoLatLongs.length == 0) || (csoEvents.length == 0)) {
+    if ((jQuery.isEmptyObject(csoLatLongs)) || (csoEvents.length == 0)) {
         return;
     }
+    alert("We're Good!");
+    for (var i = 0; i < data.length; i++) {
+
+        var color = 'blue';
+        if (i < 10) {
+            color = 'red';
+        } else {
+            continue;
+        }
+
+        var circle = L.circle([data[i].lat, data[i].lon], 500, {
+            color: color,
+            fillColor: color,
+            fillOpacity: 1
+        }).addTo(mymap);
+
+    }
 }
-    
