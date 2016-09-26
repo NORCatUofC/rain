226eaa7b518c84e18d400e4f929567eb07345c5d
sbeslow <bezzy21@yahoo.com>
2016-09-26 07:36:04 -0500

    Playing with maps and sewer data

--- sewer-overflows/js/csoData.js
+++ sewer-overflows/js/csoData.js
@@ -1,8 +1,56 @@
+var csoLatLongs = [];
+var csoEvents = [];
+
 function readCsoPoints() {
-    
-    
+    $.ajax({
+        url: "data/outfall_points.csv",
+        async: false,
+        success: function (csvd) {
+            var csoPoints = $.csv.toObjects(csvd);
+            for (var i = 0; i < csoPoints.length; i++) {
+    }
+            readCsoEvents(csoPoints);
+            alert(JSON.stringify(data));
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            alert('request failed');
+        },
+        dataType: "text",
+
+    });
+
 }
 
-function readCsoEvents() {
-    
+function readCsoEvents(csoPoints) {
+    $.ajax({
+        dataType: 'json',
+        url: '/json/cumulative_outfall_duration.json',
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                
+                var color = 'blue';
+                if (i < 10) {
+                    color = 'red';
+                }
+                else {
+                    continue;
+                }
+
+                var circle = L.circle([data[i].lat, data[i].lon], 500, {
+                    color: color,
+                    fillColor: color,
+                    fillOpacity: 1
+                }).addTo(mymap);
+
+            }
+        }
+    });
+
 }
+
+function drawMap() {
+    if ((csoLatLongs.length == 0) || (csoEvents.length == 0)) {
+        return;
+    }
+}
+    
